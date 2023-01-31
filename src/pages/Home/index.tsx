17760b72165ddc4dd094a6a26db4afca10bf9ded
Maycon Silva <mayconslv.contato@gmail.com>
2023-01-31 01:51:32 -0300

    criando rotas e páginas

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,8 @@
-import { Header } from "../../components/Header"
 import { Intro } from "../../components/Intro"
 
 export function Home() {
 	return(
 		<div>
-			<Header />
       <Intro />
 		</div>
 	)
