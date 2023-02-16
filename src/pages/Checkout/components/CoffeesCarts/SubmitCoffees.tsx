14a784455160535a35139008b8692afd8a6ea4e5
Maycon Silva <mayconslv.contato@gmail.com>
2023-02-16 07:53:19 -0300

    total items price value

--- src/pages/Checkout/components/CoffeesCarts/SubmitCoffees.tsx
+++ src/pages/Checkout/components/CoffeesCarts/SubmitCoffees.tsx
@@ -1,21 +1,23 @@
+import { useContext } from 'react'
 import { ConfirmButton } from '../../../../components/ConfirmButton'
+import { CartContext } from '../../../../contexts/cartContext'
 import { SubmitCoffeesContainer, TotalPrice } from './sytles'
+import { FormatedMoney } from '../../../../utils/FormatedMoney'
 
 export function SubmitCoffees() {
-  // const { cartItems } = useContext(CartContext)
-  // const totalItemsPrice = cartItems.reduce(function (
-  //   acumulator: number,
-  //   price: number,
-  // ) {
-  //   return acumulator + price
-  // })
+  const { cartItems } = useContext(CartContext)
+  const totalItemsPrice = cartItems.reduce((acumulator, total) => {
+    return acumulator + total.price * total.quantity
+  }, 0)
+  console.log(totalItemsPrice)
+  const formatedTotalItemsPrice = FormatedMoney(totalItemsPrice)
 
   return (
     <SubmitCoffeesContainer>
       <TotalPrice>
         <div>
           <p>Total de itens</p>
-          <p>R$ 29,70</p>
+          <p>R$ {formatedTotalItemsPrice}</p>
         </div>
         <div>
           <p>Entrega</p>
