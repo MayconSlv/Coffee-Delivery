b1e19267f9b4e7f75f55d18890f210225c62394e
Maycon Silva <mayconslv.contato@gmail.com>
2023-02-24 14:08:19 -0300

    CEP API function

--- src/pages/Checkout/components/AddressForm/AddressForm.tsx
+++ src/pages/Checkout/components/AddressForm/AddressForm.tsx
@@ -1,27 +1,60 @@
-import { MapPin } from 'phosphor-react'
 import { AddressTitle } from '../AddressTitle'
-import { AddressCardContainer, InputsLayout } from './styles'
 import { useTheme } from 'styled-components'
+import { useFormContext } from 'react-hook-form'
+
+import { MapPin } from 'phosphor-react'
+import { AddressCardContainer, InputsLayout } from './styles'
 
 export function AddressFormInputs() {
   const { colors } = useTheme()
+  const { register, setValue } = useFormContext()
+
+  const checkCEP = (e: any) => {
+    const cep = e.target.value.replace(/\D/g, '')
+    console.log(cep)
+
+    fetch(`https://viacep.com.br/ws/${cep}/json/`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data)
+        setValue('street', data.logradouro)
+        setValue('neighbor', data.bairro)
+        setValue('city', data.localidade)
+        setValue('uf', data.uf)
+      })
+  }
 
   return (
     <AddressCardContainer>
       <AddressTitle
-        title='Endereço de entrega'
-        subtitle='Informe o endereço onde deseja receber seu pedido'
+        title="Endereço de entrega"
+        subtitle="Informe o endereço onde deseja receber seu pedido"
         icon={<MapPin size={22} color={colors['yellow-dark']} />}
       />
 
       <InputsLayout>
-        <input type='text' className='cep' placeholder='CEP' />
-        <input type='text' className='street' placeholder='RUA' />
-        <input type='number' placeholder='Número' />
-        <input type='text' className='complement' placeholder='Complemento' />
-        <input type='text' placeholder='Bairro' />
-        <input type='text' placeholder='Cidade' />
-        <input type='text' placeholder='UF' />
+        <input
+          type="text"
+          className="cep"
+          placeholder="CEP"
+          onBlur={checkCEP}
+        />
+        <input
+          type="text"
+          className="street"
+          placeholder="Rua"
+          {...register('street')}
+        />
+        <input type="number" placeholder="Número" {...register('number')} />
+        <input
+          type="text"
+          className="complement"
+          placeholder="Complemento"
+          {...register('complement')}
+        />
+        <input type="text" placeholder="Bairro" {...register('neighbor')} />
+        <input type="text" placeholder="Cidade" {...register('city')} />
+        <input type="text" placeholder="UF" {...register('uf')} />
       </InputsLayout>
     </AddressCardContainer>
   )
--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -2,14 +2,40 @@ import { FormContainer } from './style'
 import { AddressForm } from './components/AddressForm'
 import { CoffeeCart } from './components/CoffeesCarts'
 
-export function Checkout() {
-  return (
-    <FormContainer className="container">
-      <div>
-        <AddressForm />
-      </div>
+import * as zod from 'zod'
+import { useForm, FormProvider } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
 
-      <CoffeeCart />
-    </FormContainer>
+export function Checkout() {
+  const addressFormValidationSchema = zod.object({
+    cep: zod
+      .string()
+      .min(8, 'O CEP que você diitou é inválido')
+      .max(8, 'O CEP que você diitou é inválido'),
+    street: zod.string(),
+  })
+
+  type AddressFormData = zod.infer<typeof addressFormValidationSchema>
+
+  const addressForm = useForm<AddressFormData>({
+    resolver: zodResolver(addressFormValidationSchema),
+  })
+
+  const { handleSubmit } = addressForm
+
+  function handleConfirmForm(data: AddressFormData) {
+    console.log(data)
+  }
+
+  return (
+    <FormProvider {...addressForm}>
+      <FormContainer
+        className="container"
+        onSubmit={handleSubmit(handleConfirmForm)}
+      >
+        <AddressForm />
+        <CoffeeCart />
+      </FormContainer>
+    </FormProvider>
   )
 }
